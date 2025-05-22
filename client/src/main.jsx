15ee6b8b5b3461c42fc148bb58b1155ce8f8119e
Cxvarisorka <lcxvaradze2006@gmail.com>
2025-05-22 16:27:42 +0400

    tailwind css added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext.jsx";
+import './tailwind.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
