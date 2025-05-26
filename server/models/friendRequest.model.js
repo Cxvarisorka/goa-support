42e62928c1bbbfbb4c380ac3154a27a5c0b564b0
Cxvarisorka <lcxvaradze2006@gmail.com>
2025-05-26 17:16:29 +0400

    Friend request model

---
+++ server/models/friendRequest.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const friendRequestSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'declined'],
+        default: 'pending'
+    }
+}, {
+    timestamps: true
+});
+
+const FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
+
+module.exports = FriendRequest;
