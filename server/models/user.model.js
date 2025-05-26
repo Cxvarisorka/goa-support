58f4470fadaf12a4eb26b83da6442629bf95e1a7
Cxvarisorka <lcxvaradze2006@gmail.com>
2025-05-26 17:16:52 +0400

    Username and friends arr prop added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,9 +4,31 @@ const userSchema = new mongoose.Schema({
     fullname: {
         type: String,
         required: true,
-        unique: true,
-        trim: true
+        trim: true,
+        validate: {
+            validator: function (v) {
+                    // მხოლოდ ქართული ასოები და ერთი space (მაგ. ლუკა ცხვარაძე)
+                    return /^[ა-ჰ]+\s[ა-ჰ]+$/.test(v);
+                },
+            message: props => `${props.value} არ არის ვალიდური სახელი და გვარი (მაგ: ლუკა ცხვარაძე)`
+        }
     },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6,
+        maxlength: 10,
+        validate: {
+            validator: function (v) {
+            // მხოლოდ ინგლისური ასოები და რიცხვები
+                return /^[a-zA-Z0-9]+$/.test(v);
+            },
+            message: props => `${props.value} უნდა შეიცავდეს მხოლოდ ინგლისურ სიმბოლოებს და რიცხვებს`
+        }
+    },
+
     email: {
         type: String,
         required: true,
@@ -21,14 +43,18 @@ const userSchema = new mongoose.Schema({
         enum: ['admin', 'editor', 'user'],
         default: 'user'
     },
-    isVerified: { // როდესაც მომმხრაბელეი გაივლის რეგისტრაციას, იმეილზე მიუვა კოდი (სანამ არ დაადასტურებს იქამდე შეუძლებელი იქნება აქაუნთზე შესვლა)
+    isVerified: { 
         type: Boolean,
         default: false
     },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
 }, {
     timestamps: true,
-  }
-);
+});
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
