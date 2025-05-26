6e76227e2205f2f7a725cf1ea625aa4464edefe1
Cxvarisorka <lcxvaradze2006@gmail.com>
2025-05-26 17:18:06 +0400

    get use profile and search router added

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 // controllers
-const { register, verifyEmail, login, profile, changePassword, logout } = require("../controllers/user.controller.js");
+const { register, verifyEmail, login, myProfile, changePassword, logout, searchUser, userProfile } = require("../controllers/user.controller.js");
 
 // middlewares
 const verifyToken = require("../middlewares/auth.js");
@@ -11,8 +11,10 @@ const userRouter = express.Router();
 userRouter.post("/register", express.json(), register);
 userRouter.get("/verify-email", verifyEmail);
 userRouter.post("/login", express.json(), login);
-userRouter.get("/profile", verifyToken, profile);
+userRouter.get("/my-profile", verifyToken, myProfile);
 userRouter.post("/change-password", verifyToken, express.json(), changePassword);
 userRouter.post("/logout", verifyToken, logout);
+userRouter.get("/search", verifyToken, searchUser);
+userRouter.get("/profile/:userId", verifyToken, userProfile);
 
 module.exports = userRouter;
