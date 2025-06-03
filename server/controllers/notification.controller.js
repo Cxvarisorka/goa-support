00020fe7c40d6dcb01c32d7c7dc89d773dc1f473
Cxvarisorka <lcxvaradze2006@gmail.com>
2025-06-03 18:19:28 +0400

    Notification controller added

---
+++ server/controllers/notification.controller.js
@@ -0,0 +1,5 @@
+// Models (მონაცემთა ბაზის სექცია)
+const User = require("../models/user.model.js");
+const VerificationToken = require("../models/token.model.js");
+const FriendRequest = require("../models/friendRequest.model.js");
+
